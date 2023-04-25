8a318efcd25f58e2b6235b04bc83008a29bba9ab
Miraj <mirajiik@yandex.ru>
2023-04-25 17:02:33 +0500

    Добавьте файлы проекта.

---
+++ AndroidOracle/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidOracle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        bool firstStart = true;
+        public async void FadeAnswer(object sender, PropertyChangedEventArgs e)
+        {
+
+            if (e.PropertyName == "Text")
+            {
+                if (firstStart)
+                {
+                    firstStart = false;
+                    return;
+                }
+                Label cell = ((Label)sender);
+                cell.Opacity = 0;
+                await cell.FadeTo(1, 2000);
+            }
+        }
+        public async void AnswersAnimate(object sender, EventArgs e)
+        {
+            View cell = ((ViewCell)sender).View;
+            cell.Opacity = 0;
+            await cell.FadeTo(1, 1000);
+        }
+    }
+}
---
+++ AndroidOracle/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AndroidOracle
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+            GenerateAnswersCommand = new Command(GenerateAnswers);
+            HistoryAnswers = new ObservableCollection<string>();
+        }
+
+        private string answer = "";
+        public string Answer 
+        {
+            get => answer;
+            set => Set(ref answer, value);
+        }
+        private bool answerSwitcher = false;
+        public bool AnswerSwitcher
+        {
+            get => answerSwitcher;
+            set => Set(ref answerSwitcher, value);
+        }
+        public ObservableCollection<string> HistoryAnswers { get; set; }
+        public ICommand GenerateAnswersCommand { get;  }
+
+        //Random rnd = new Random();
+        RandomNumberGenerator rnd = RandomNumberGenerator.Create();
+        byte[] result = new byte[1];
+        private async void GenerateAnswers()
+        { 
+            int y = 0, n = 0;
+            HistoryAnswers.Clear();
+            Answer = "";
+            if (answerSwitcher)
+            {
+                while ((y != 3) && (n != 3))
+                {
+                    rnd.GetBytes(result);
+                    if (result[0] % 2 == 0)
+                    {
+                        n++;
+                        HistoryAnswers.Add("Нет");
+                    }
+                    else
+                    {
+                        y++;
+                        HistoryAnswers.Add("Да");
+                    }
+                    if ((y != 3) && (n != 3))
+                        await Task.Delay(1000);
+                    
+                }
+            }
+            else
+            {
+                rnd.GetBytes(result);
+                if (result[0] % 2 == 0)
+                    n = 3;
+                else
+                    y = 3;
+            }
+            Answer = n == 3 ? "Нет" : "Да";            
+        }
+    }
+}
